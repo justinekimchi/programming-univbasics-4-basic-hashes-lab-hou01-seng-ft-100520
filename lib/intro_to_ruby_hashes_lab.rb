c3cf48a142af73930ae2def5a0fb0336af7f415c
Justine Manaloto <justine.kim.manaloto@gmail.com>
2020-09-06 19:16:36 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,9 +1,9 @@
 def new_hash
-  # return an empty hash
+  new_hash=Hash.new
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+ {:favorite_pet => "Perry"}
 end
 
 def pioneer
